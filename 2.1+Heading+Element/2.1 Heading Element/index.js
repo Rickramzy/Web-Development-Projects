2cad5d8d54f97b8bf096122e5d611d04fb2cdde8
Raphael Oguejiofor <raphael.oguejiofor@outsourceglobal.net>
2025-12-22 21:42:41 +0100

    New updates on Javascript Lessons

---
+++ 2.1+Heading+Element/2.1 Heading Element/index.js
@@ -0,0 +1,23 @@
+// let nam = "Ralph";
+// let message = "Hello";
+// alert(`${message}, ${nam}!`);
+// alert(message + " there," + " " + nam);
+
+
+// // console.log(name);
+
+// Node.js version (runs in VS Code terminal)
+
+let nam = "Ralph";
+let message = "Hello";
+
+// Template literal
+console.log(`${message}, ${nam}!`);
+
+// String concatenation
+console.log(message + " there, " + nam);
+// alert(`Hello`)
+
+document.querySelector("h1").innerHTML = "Goodbye!";
+
+
---
+++ DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -0,0 +1,11 @@
+// document.querySelectorAll(".list")[2].textContent = "Ralph";
+// // document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Ralph";
+// // document.getElementsByClassName("list")[1].innerHTML = "Ralph";
+// document.querySelectorAll(".list a").style.color = "red";
+
+document.querySelector("button").style.backgroundColor = "yellow";
+document.querySelector("button").style.color = "white";
+document.querySelector("button").style.fontSize = "24px";
+document.querySelector("button").style.padding = "10px 20px";
+document.querySelector("button").style.border = "none";
+document.querySelector("h1").classList.add("letter");
