de6410ef40ed99ce71839b46adc7da07ee83d310
Raphael Oguejiofor <raphael.oguejiofor@outsourceglobal.net>
2026-01-09 22:18:29 +0100

    Javascript projects

---
+++ JQuery/index.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+    $("h1").css("color", "red");
+    $("button").click(function() {
+        $("button").css("background-color", "gray");
+        $("button").css("color", "white");
+    });
+// });
