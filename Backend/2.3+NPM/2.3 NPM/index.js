a6bdcbb5fef3f1e1a37421e3905909e2833dcd8d
Raphael Oguejiofor <raphael.oguejiofor@outsourceglobal.net>
2026-01-27 21:17:29 +0100

    Backend lessons and projects update

---
+++ Backend/2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,13 @@
+// let generateName = require('sillyname');
+
+// import generateName from "sillyname";
+// let sillyName = generateName();
+
+// console.log(`My name is ${sillyName}`);
+
+//Superheros names generator
+
+
+import {randomSuperhero} from 'superheroes';
+
+console.log(`I am ${randomSuperhero()}`);
